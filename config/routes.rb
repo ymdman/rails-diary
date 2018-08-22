75da61d10e29ef49e8a896b1c3a8642de3097812
ymdman <revitalized@icloud.com>
2018-08-22 21:10:59 +0900

    ひな型の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :diaries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/users', to: 'users#index'
   get '/users/new'

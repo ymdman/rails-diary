934df928547fcbf4b0d852ed4f17985dd44f2e5b
ymdman <revitalized@icloud.com>
2018-09-04 19:25:44 +0900

    独自の検証ルールを実装

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,16 @@
 class Diary < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255}
   validates :body, presence: true, length: { maximum: 10_000}
+  validate :proper_title_and_body
+
+  private
+
+    def proper_title_and_body
+      unless title.starts_with?('今日')
+        errors.add(:title, 'は「今日」から初めてください')
+      end
+      unless body.ends_with?('。')
+        errors.add(:body, 'は句点（。）で終了してください')
+      end
+    end
 end

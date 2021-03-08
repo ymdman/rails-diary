bb101bc2d376b4647152600de191e144200b7c31
ymdman <revitalized@icloud.com>
2021-03-08 22:17:18 +0900

    feat: roouting setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @str = 'Hello World!!!!!'
     render temlate: 'users/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to: 'root#index'
   get '/users', to: 'users#index'
+  get '/users/new'
 end

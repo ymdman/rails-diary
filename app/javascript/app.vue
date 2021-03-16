207638cef48e1eb3e293180e2d03dd3e6f66bbdf
ymdman <revitalized@icloud.com>
2021-03-16 18:58:24 +0900

    feat: add footer

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -2,14 +2,16 @@
   <div id="app">
     <TheHeader />
     <p>{{ message }}</p>
+    <TheFooter />
   </div>
 </template>
 
 <script lang='ts'>
 import TheHeader from './packs/components/TheHeader.vue'
+import TheFooter from './packs/components/TheFooter.vue'
 
 export default {
-  components: { TheHeader },
+  components: { TheHeader, TheFooter },
   data: function () {
     return {
       message: "Hello Vue!"
---
+++ app/javascript/packs/components/TheFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>TheFooter</footer>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+
+  data: function () {
+    return {
+      test: '',
+    }
+  }
+}
+</script>

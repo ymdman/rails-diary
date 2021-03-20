bdf684f53dd5893cda905e36af0879873082a6d3
ymdman <revitalized@icloud.com>
2021-03-20 15:51:29 +0900

    feat: add scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :diaries
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'root#index'
   get '/users', to: 'users#index'
